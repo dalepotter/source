3de5c719446a4cf3d16c0457273775b22ea3a7bf
Zara Rahman <zara.rahman@okfn.org>
2014-05-28 17:35:05 +0200

    Fix image bug

--- source/s3utils.py
+++ source/s3utils.py
@@ -1,20 +1,15 @@
-from django.core.files.storage import get_storage_class
-
 from storages.backends.s3boto import S3BotoStorage
 
-class CachedS3BotoStorage(S3BotoStorage):
+class StaticRootS3BotoStorage(S3BotoStorage):
     """
     S3 storage backend that saves the files locally, too.
     """
     def __init__(self, *args, **kwargs):
-        super(CachedS3BotoStorage, self).__init__(*args, **kwargs)
-        self.local_storage = get_storage_class(
-            "compressor.storage.CompressorFileStorage")()
+    	kwargs['location'] = 'static'
+        super(StaticRootS3BotoStorage, self).__init__(*args, **kwargs)
 
-    def save(self, name, content):
-        name = super(CachedS3BotoStorage, self).save(name, content)
-        self.local_storage._save(name, content)
-        return name
+class MediaRootS3BotoStorage(S3BotoStorage):
 
-StaticRootS3BotoStorage = lambda: CachedS3BotoStorage(location='static')
-MediaRootS3BotoStorage  = lambda: S3BotoStorage(location='media')
+   def __init__(self, *args, **kwargs):
+		kwargs['location'] = 'media'
+        super(MediaRootS3BotoStorage, self).__init__(*args, **kwargs)
